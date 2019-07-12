74c0144de7a5f5fe2100b9c21895b7e13be60eea
Your zhangheng <1356770740.com>
2019-07-12 13:48:57 +0800

    7月12日
    安装react-navigation

--- MyRNApp/index.js
+++ MyRNApp/index.js
@@ -4,7 +4,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+
+import App from './src/tabbarNav';
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- RNProject.md
+++ RNProject.md
@@ -1,2 +1,6 @@
 注意事项：
 1、项目创建后，运行项目时需npm start
+2、安装react-navigation步骤：
+(1)、npm install --save react-navigation
+(2)、npm install --save react-native-gesture-handler
+(2)、react-native link 
